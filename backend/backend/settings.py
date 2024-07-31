02b8c970d7ab8b1e7e0de6a385e37c4e8cec5b61
Fishappy0 <fishappy0@gmail.com>
2024-07-31 21:47:43 +0700

    Update ssl status

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_REFERRER_POLICY = "same-origin"
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
